5eca746101e33492bd873f25c04892bdaba7df18
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-06-10 13:28:28 +0200

    Actualicé el config

--- include/config.h
+++ include/config.h
@@ -3,14 +3,14 @@
 const char apWifiName[] = "gateway_ha";
 const char wifiInitialApPassword[] = "12345678";
 //-- VARIABLES DE LOS PARAMETROS A CONFIGURAR
-char mqttServerValue[STRING_LEN];
-char mqttServerPortValue[STRING_LEN];
-char mqttUserNameValue[STRING_LEN];
-char mqttUserPasswordValue[STRING_LEN];
-char mqttTopicValue[STRING_LEN];
+char mqttServerValue[STRING_LEN] = "aps2023.is-a-student";
+char mqttServerPortValue[STRING_LEN] = "2883";
+char mqttUserNameValue[STRING_LEN] = "admin";
+char mqttUserPasswordValue[STRING_LEN] = "admin";
+char mqttTopicValue[STRING_LEN] = "aps2023/Proyecto7";
 char mqttAutoSend[STRING_LEN];
 //-- CONFIGURACIÓN SERIAL PORT BASCULA
-char serial1Baudios[STRING_LEN];
+char serial1Baudios[STRING_LEN] = "9600";
 char modeloBascula[STRING_LEN];
 char udBascula[STRING_LEN];
 //-- CONFIGURACIÓN IP FIJA AÑADIR A LA CONFIGURACION DE PARAMETROS
